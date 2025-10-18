254c9085710192b1f20afe3118a1b1c66a57ee9d
aesyadityasafina <aesyaditya6@gmail.com>
2025-10-18 16:35:15 +0700

    Create variabel.php

---
+++ P1/Latihan/variabel.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+        <TITLE>P1</TITLE>
+    </HEAD>
+        <BODY>
+            <?php
+            $nama = "Aesya Ditya Safina";                         
+            echo "$nama berkata \"Katakan namamu!\"";
+            ?>
+        </BODY>
+</HTML>                                                     
