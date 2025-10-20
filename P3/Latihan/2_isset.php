3c454de1f964776ec8767202e7aeeb96889e59c2
aesyadityasafina <aesyaditya6@gmail.com>
2025-10-20 20:59:59 +0700

    Add files via upload

---
+++ P3/Latihan/2_isset.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Demo Variabel</title>
+</head>
+<body>
+
+<?php
+
+$bil = 10;
+var_dump($bil);
+// Output: int(10)
+
+$var = "";
+var_dump($var);
+// Output: string(0) ""
+
+$var = null;
+var_dump($var);
+// Output: NULL
+
+?>
+
+</body>
+</html>
---
+++ P3/Latihan/2_variabel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Demo Variabel</title>
+</head>
+
+<body>
+
+<?php
+
+// Deklarasi dan inisialisasi
+$bil = 10;
+
+echo $bil;
+
+?>
+
+</body>
+</html>
